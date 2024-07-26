12c50f1d122215a1f62c58333a16131528f2c116
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-07-25 22:53:51 -0400

    Modificamos el program para las inyecciones.

--- MediSoft/Program.cs
+++ MediSoft/Program.cs
@@ -11,7 +11,7 @@ namespace MediSoft
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args); 
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
