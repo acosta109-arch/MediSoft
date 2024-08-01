24c4d6fee9fe48a73937100deb8108b5329a4f0f
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-07-31 21:03:30 -0400

    Correciones en DetalleDoctores.

--- MediSoft/Models/DetalleDoctores.cs
+++ MediSoft/Models/DetalleDoctores.cs
@@ -1,14 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MediSoft.Models;
 
 public class DetalleDoctores
 {
     [Key]
-    public int DetalleDoctoresId { get; set; }
+    public int DetalleId { get; set; }
 
     [Required(ErrorMessage = "El campo especialidad es obligatorio.")]
-    public string Especialidad { get; set; }
+    public string DiasDisponibilidad { get; set; }
+
+    [ForeignKey("DoctorId")]
+    [Required(ErrorMessage = "Campo doctor es obligatorio.")]
+    public int DoctorId { get; set; }
 
 }
  
