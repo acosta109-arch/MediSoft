568fdaaaf29450e2a607c0f879b48ae9d726e526
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-08-05 11:22:48 -0400

    Agregamos el identity.

---
+++ MediSoft/Data/Identity/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSoft.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "Usuarios");
+        });
+
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Roles");
+        });
+
+    }
+}
